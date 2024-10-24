Fixed masking

eurocropsml/dataset/base.py
@@ -87,13 +87,14 @@ class LabelledData(NamedTuple):
         return self.data_item, self.label
 
 
-def custom_collate_fn(batch: Sequence[LabelledData]) -> LabelledData:
+def custom_collate_fn(batch: Sequence[LabelledData], padding_value: int = -1) -> LabelledData:
     """Collate function for batch creation within data loader.
 
     Used to create batches from a dataset's DataItem.
 
     Args:
         batch: List of DataItem from dataset
+        padding_value: Value used for padding.
 
     Returns:
         New DataItem with batched data.
@@ -112,7 +113,7 @@ def custom_collate_fn(batch: Sequence[LabelledData]) -> LabelledData:
         tensor_name: (
             torch.stack(tensors)
             if tensor_stackability[tensor_name]
-            else pad_sequence(tensors, batch_first=True, padding_value=-1)
+            else pad_sequence(tensors, batch_first=True, padding_value=padding_value)
         )
         for tensor_name, tensors in batch_tensors.items()
     }
@@ -123,7 +124,7 @@ def custom_collate_fn(batch: Sequence[LabelledData]) -> LabelledData:
     ):
         batched_tensors["label"] = torch.concat(batch_tensors["label"], 0)
 
-    if (pad_mask := batched_tensors["data"].eq(-1)).any():
+    if (pad_mask := batched_tensors["data"].le(padding_value)).any():
         if (aug_mask := batched_tensors.get("mask")) is not None:
             # if pad_mask.dim (B, T, C) != aug_mask.dim (B, T, C) | (B, T)
             # => aug_mask.dim is (B, T), thus pad_mask need to be converted