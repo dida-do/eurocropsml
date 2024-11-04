e73356f6107964e992d43e054c723c3fba2cda6f
jreuss <joana.reuss@tum.de>
2024-11-04 17:48:24 +0100

    adding range before scaling and normalization

--- eurocropsml/dataset/dataset.py
+++ eurocropsml/dataset/dataset.py
@@ -159,9 +159,11 @@ class EuroCropsDataset(Dataset[LabelledData]):
         # normalization and scaling to (0,1]
         if self.config.normalize:
             if "S1" in np_data_dict:
+                # range before normalization and scaling: [-50.0,1.0]
                 normalized_s1 = np_data_dict["S1"] * NORMALIZING_FACTOR_S1
                 np_data_dict["S1"] = (normalized_s1 + 1) / 2 + EPSILON
             if "S2" in np_data_dict:
+                # range before normalization and scaling: [0.0, 1.0e-4]
                 normalized_s2 = np_data_dict["S2"] * NORMALIZING_FACTOR_S2
                 np_data_dict["S2"] = normalized_s2 * (1 - EPSILON) + EPSILON
 
