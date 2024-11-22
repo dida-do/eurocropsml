1c749e870b5c778f5d38057054f58d920d324749
jsreuss <joana.reuss@tum.de>
2024-11-22 14:56:53 +0000

    Removed conversion back to string

--- eurocropsml/acquisition/clipping/utils.py
+++ eurocropsml/acquisition/clipping/utils.py
@@ -217,10 +217,6 @@ def _merge_clipper(
     logger.info(f"Starting merging of DataFrames for {month}...")
     df_list: list = [file for file in clipped_output_dir.iterdir() if "Final_" in file.name]
 
-    full_df.columns = [full_df.columns[0]] + [
-        pd.to_datetime(col).strftime("%Y-%m-%d") for col in full_df.columns[1:]
-    ]
-
     # setting parcel_id column to index
     full_df.set_index(parcel_id_name, inplace=True)
     for file in tqdm(df_list):
